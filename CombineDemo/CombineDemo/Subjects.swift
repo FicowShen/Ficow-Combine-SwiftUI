ea67a6fe6566f1256f66b43b06a8025ab3138c52
Ficow Shen <ficowshen@hotmail.com>
2020-09-12 23:09:25 +0800

    Update subject demo;

--- CombineDemo/CombineDemo/Subjects.swift
+++ CombineDemo/CombineDemo/Subjects.swift
@@ -13,22 +13,67 @@ final class SubjectsDemo {
     
     private var cancellable1: AnyCancellable?
     private var cancellable2: AnyCancellable?
+    private var cancellable3: AnyCancellable?
+    private var cancellable4: AnyCancellable?
     
-    private let currentValueSubject = CurrentValueSubject<Void, Never>(())
+    private let currentValueSubject = CurrentValueSubject<Int, Never>(1)
     private let passThroughtSubject = PassthroughSubject<Int, Never>()
+
+    private let optionalCurrentValueSubject = CurrentValueSubject<Int?, Never>(nil)
+    
     func run() {
-        currentValueSubjectDemo()
-        passThroughtSubjectDemo()
-        
-        currentValueSubject.send(())
-        passThroughtSubject.send(5)
+//        passThroughtSubjectDemo()
+//        currentValueSubjectDemo()
+//        subjectSubscriber()
     }
     
     private func currentValueSubjectDemo() {
-        cancellable1 = currentValueSubject.sink { log("sink:", $0) }
+        cancellable1 = currentValueSubject
+            .sink { [unowned self] in
+                print(#function, $0)
+                print("Value of currentValueSubject:", self.currentValueSubject.value)
+            }
+        currentValueSubject.send(2)
+        currentValueSubject.send(3)
     }
     
     private func passThroughtSubjectDemo() {
-        cancellable2 = passThroughtSubject.sink { log("sink:", $0) }
+        cancellable2 = passThroughtSubject
+            .sink {
+                print(#function, $0)
+            }
+        passThroughtSubject.send(1)
+        passThroughtSubject.send(2)
+        passThroughtSubject.send(3)
+    }
+    
+    private func subjectSubscriber() {
+        cancellable3 = optionalCurrentValueSubject
+            .sink {
+                print(#function, $0)
+            }
+        cancellable4 = [1, 2, 3].publisher
+            .subscribe(optionalCurrentValueSubject) //
     }
 }
+
+
+enum ViewModelWithSubjectDemo {
+    struct Model {
+        let id: UUID
+        let name: String
+    }
+    
+    final class ViewModel {
+        private let modelSubject = CurrentValueSubject<Model?, Never>(nil)
+        var modelPublisher: AnyPublisher<Model?, Never> {
+            return modelSubject.eraseToAnyPublisher()
+        }
+        
+        func updateName(_ name: String) {
+            modelSubject.send(.init(id: UUID(), name: name))
+        }
+    }
+}
+
+
